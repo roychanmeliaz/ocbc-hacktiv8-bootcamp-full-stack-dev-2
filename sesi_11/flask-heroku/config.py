d8850c120eb278e4727bdb96414ddbece5106e9a
roychanmeliaz <roychan.meliaz@gmail.com>
2021-11-15 12:26:56 +0700

    update session 11

---
+++ sesi_11/flask-heroku/config.py
@@ -0,0 +1,23 @@
+import os
+import connexion
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Create the Connexion application instance
+connex_app = connexion.App(__name__, specification_dir=basedir)
+
+# Get the underlying Flask app instance
+app = connex_app.app
+
+# Configure the SQLAlchemy part of the app instance
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://cmptnafmfeiebz:1a7aca0a217cbd585a084c689dafdf4f8c3e019dd20718d9e5a09e8f26f60624@ec2-52-204-72-14.compute-1.amazonaws.com:5432/d5q9773cg061a1'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Create the SQLAlchemy db instance
+db = SQLAlchemy(app)
+
+# Initialize Marshmallow
+ma = Marshmallow(app)
